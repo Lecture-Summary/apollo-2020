56ba889735f1bf3896da0340dfa5a631ae318e32
HoJin9622 <kiss0104040@gmail.com>
2020-06-17 01:33:10 +0900

    GET MOVIE Query 2

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
 const GET_MOVIE = gql`
-  query getMovie($id: Int) {
+  query getMovie($id: Int!) {
     movie(id: $id) {
       id
       title
@@ -14,7 +15,15 @@ const GET_MOVIE = gql`
 `;
 
 export default () => {
-  const { id } = useParams();
-  console.log(id);
-  return "Detail";
+  let { id } = useParams();
+  id = parseInt(id);
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id },
+  });
+  if (loading) {
+    return "loading";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
 };
